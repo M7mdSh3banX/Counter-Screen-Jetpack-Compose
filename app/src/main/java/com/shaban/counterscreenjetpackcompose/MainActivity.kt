2b630a664f3844eeef133715a3d1374e7a3a2a16
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-03 00:08:39 +0300

    implement counter screen

---
+++ app/src/main/java/com/shaban/counterscreenjetpackcompose/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.shaban.counterscreenjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shaban.counterscreenjetpackcompose.screen.CounterScreen
+import com.shaban.counterscreenjetpackcompose.ui.theme.CounterScreenJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CounterScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/shaban/counterscreenjetpackcompose/screen/CounterScreen.kt
@@ -0,0 +1,58 @@
+package com.shaban.counterscreenjetpackcompose.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CounterScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        var counter by remember { mutableStateOf(0) }
+
+        Text(text = counter.toString(), fontSize = 96.sp)
+        Row {
+            Button(
+                modifier = Modifier.size(96.dp),
+                onClick = { counter-- },
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Text(text = "-", fontSize = 48.sp)
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                modifier = Modifier.size(96.dp),
+                onClick = { counter++ },
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Text(text = "+", fontSize = 48.sp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCounterScreen() {
+    CounterScreen()
+}
